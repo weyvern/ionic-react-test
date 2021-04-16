56d95027bd67845a8ee2d1aab6a4ef67a25bda96
Jorge Paul <jserranopaul@gmail.com>
2021-04-16 11:16:17 +0200

    ionic react

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +11,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
